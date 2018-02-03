ff620a9e23027054bfecac7faaa63951d0d75790
ernestguevarra <ernestgmd@gmail.com>
2018-02-03 09:28:56 +0000

    update getCohortWGS scripts and documentation

--- R/getCohortWGS.R
+++ R/getCohortWGS.R
@@ -30,14 +30,14 @@
 #'
 #' @examples
 #'   # apply getWGS to first child in sample data anthro1
-#'   wazAll <- getCohortWGS(data = anthro1
+#'   wazAll <- getCohortWGS(data = anthro1,
 #'                          sexObserved = "sex",
 #'                          firstPart = "weight",
 #'                          secondPart = "age",
 #'                          index = "wfa")
 #'   wazAll
 #'
-#'   hazAll <- getCohortWGS(data = anthro1
+#'   hazAll <- getCohortWGS(data = anthro1,
 #'                          sexObserved = "sex",
 #'                          firstPart = "height",
 #'                          secondPart = "age",

665de306701f69197a336ecadb54ae7711e819a7
ernestguevarra <ernestgmd@gmail.com>
2018-02-04 07:03:44 +0000

    add error message tests for getAllWGS

--- tests/testthat/test_getAllWGS.R
+++ tests/testthat/test_getAllWGS.R
@@ -103,3 +103,35 @@ test_that("names is waz", {
   expect_equal(names(waz), "waz")
 })
 
+test_that("produces error", {
+  expect_error(getAllWGS(data = anthro1, sex = anthro1$sex, weight = anthro1$weight, height = anthro1$height, age = anthro1$age, index = "wfa"),
+               "If data not provided, sex, weight and age must be character values. Try again")
+})
+
+test_that("produces error", {
+  expect_error(getAllWGS(sex = "sex", weight = "weight", height = "height", age = "age", index = "hfa"),
+               "sex, height and age must be numeric. Try again.")
+})
+
+haz <- getAllWGS(sex = anthro1$sex,
+                 weight = anthro1$weight,
+                 height = anthro1$height,
+                 age = anthro1$age,
+                 index = "hfa")
+
+test_that("haz is a data.frame", {
+  expect_is(haz, "data.frame")
+})
+
+test_that("haz is numeric", {
+  expect_true(is.numeric(haz[[1]]))
+})
+
+test_that("names is haz", {
+  expect_equal(names(haz), "haz")
+})
+
+test_that("produces error", {
+  expect_error(getAllWGS(data = anthro1, sex = anthro1$sex, weight = anthro1$weight, height = anthro1$height, age = anthro1$age, index = "hfa"),
+               "If data not provided, sex, height and age must be character values. Try again")
+})

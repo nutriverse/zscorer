f69af0e04f4410370aa99572d92871782a6c181e
Ernest Guevarra <ernest.guevarra@post.harvard.edu>
2018-05-27 14:55:07 +0100

    update hcfa_chart_data.R

--- data-raw/hcfa_chart_data.R
+++ data-raw/hcfa_chart_data.R
@@ -31,24 +31,27 @@ get_hcfa_zchart <- function(baseurl = "http://www.who.int/childgrowth/standards/
   #
   #
   #
-  names(temp) <- c("sex", "month", "day", "-4SD", "-3SD", "-2SD", "-1SD", "0", "1SD", "2SD", "3SD", "4SD")
+  names(temp) <- c("sex", "month", "day", "-4SD", "-3SD", "-2SD", "-1SD", "0",
+                   "1SD", "2SD", "3SD", "4SD")
   #
   #
   #
-  hcfa_chart <- tidyr::gather(data = temp, key = "sd_type", value = "sd_value", names(temp)[4]:names(temp)[ncol(temp)])
+  hcfa_chart <- tidyr::gather(data = temp, key = "sd_type", value = "sd_value",
+                              names(temp)[4]:names(temp)[ncol(temp)])
   names(hcfa_chart) <- c("sex", "month", "day", "sd_type", "sd_value")
-  hcfa_chart$sd_type <- factor(hcfa_chart$sd_type, levels = c("4SD", "3SD", "2SD", "1SD", "0", "-1SD", "-2SD", "-3SD", "-4SD"))
+  hcfa_chart$sd_type <- factor(hcfa_chart$sd_type,
+                               levels = c("4SD", "3SD", "2SD", "1SD", "0",
+                                          "-1SD", "-2SD", "-3SD", "-4SD"))
   #
   #
   #
   return(hcfa_chart)
 }
-
-
 #
 #
 #
-xx <- get_hcfa_zchart()
+hcfa_zchart <- get_hcfa_zchart()
+write.csv(hcfa_zchart, "data-raw/charts/hcfa_zchart.csv", row.names = FALSE)
 
 
 ################################################################################
@@ -155,7 +158,8 @@ get_hcfa_pchart <- function(baseurl = "http://www.who.int/childgrowth/standards/
   #
   #
   #
-  hcfa_chart <- tidyr::gather(data = temp, key = "p_type", value = "p_value", names(temp)[7]:names(temp)[ncol(temp)])
+  hcfa_chart <- tidyr::gather(data = temp, key = "p_type", value = "p_value",
+                              names(temp)[7]:names(temp)[ncol(temp)])
   names(hcfa_chart) <- c("sex", "month", "day", "l", "m", "s", "p_type", "p_value")
   hcfa_chart$p_type <- factor(hcfa_chart$p_type,
                               levels = c("0.10th", "1st", "3rd", "5th", "10th",
@@ -166,12 +170,11 @@ get_hcfa_pchart <- function(baseurl = "http://www.who.int/childgrowth/standards/
   #
   return(hcfa_chart)
 }
-
-
 #
 #
 #
-xx <- get_hcfa_pchart()
+hcfa_pchart <- get_hcfa_pchart()
+write.csv(hcfa_pchart, "data-raw/charts/hcfa_pchart.csv", row.names = FALSE)
 
 
 ################################################################################

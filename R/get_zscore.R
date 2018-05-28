784301a073ea8cb60add746e61c079c083687387
Ernest Guevarra <ernest@validmeasures.org>
2018-05-28 20:36:40 +0100

    rename get_z.R

--- R/get_zscore.R
+++ R/get_zscore.R
@@ -1,6 +1,6 @@
 ################################################################################
 #
-#' get_z
+#' get_zscore
 #'
 #' Calculate z-scores for length/height-for-age (HAZ), weight-for-age (WAZ),
 #' weight-for-height/length (WHZ), BMI-for-age (BAZ), head circumference-for-age
@@ -19,10 +19,12 @@
 #'     (specifies the required index)
 #' @return z-score of the anthropometric index selected
 #'
-#' @section The function fails messily when \code{secondPart} is outside of the range
+#' @section Warning:
+#' The function fails messily when \code{secondPart} is outside of the range
 #' given in the WGS reference (i.e. 45 to 120 cm for height and 0 to 60 months
 #' for age). It is up to you to check the ranges of your data.
 #'
+#' @section Reminders:
 #' The reference data for W/H assumes supine length is used for children with a
 #' standing height below 85cm
 #'
@@ -35,24 +37,24 @@
 #' The function requires reference data \code{wgsData} included in this package
 #'
 #' @examples
-#'   # apply \code{get_z()} to a make believe 52 month old male child with weight of
+#'   # apply \code{get_zscore()} to a make believe 52 month old male child with weight of
 #'   # 14.6 kg and height of 98.0 cm
-#'   waz <- get_z(sexObserved = 1,     # 1 = Male / 2 = Female
-#'                firstPart = 14.6,    # Weight in kilograms
-#'                secondPart = 52,     # Age in whole months
-#'                index = "wfa")       # Anthropometric index (weight-for-age)
+#'   waz <- get_zscore(sexObserved = 1,     # 1 = Male / 2 = Female
+#'                     firstPart = 14.6,    # Weight in kilograms
+#'                     secondPart = 52,     # Age in whole months
+#'                     index = "wfa")       # Anthropometric index (weight-for-age)
 #'   waz
 #'
-#'   haz <- get_z(sexObserved = 1,
-#'                firstPart = 98,      # Height in centimetres
-#'                secondPart = 52,
-#'                index = "hfa")       # Anthropometric index (height-for-age)
+#'   haz <- get_zscore(sexObserved = 1,
+#'                     firstPart = 98,      # Height in centimetres
+#'                     secondPart = 52,
+#'                     index = "hfa")       # Anthropometric index (height-for-age)
 #'   haz
 #'
-#'   whz <- get_z(sexObserved = 1,
-#'                firstPart = 14.6,
-#'                secondPart = 98,
-#'                index = "wfh")       # Anthropometric index (weight-for-height)
+#'   whz <- get_zscore(sexObserved = 1,
+#'                     firstPart = 14.6,
+#'                     secondPart = 98,
+#'                     index = "wfh")       # Anthropometric index (weight-for-height)
 #'   whz
 #'
 #' @export
@@ -60,7 +62,7 @@
 #
 ################################################################################
 
-get_z <- function(sexObserved, firstPart, secondPart, index) {
+get_zscore <- function(sexObserved, firstPart, secondPart, index) {
 
   if(is.na(sexObserved) | is.na(firstPart) | is.na(secondPart)) { return(NA) }
 

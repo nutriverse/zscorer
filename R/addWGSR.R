0905fe5158517f7c565da2750cd4aa24cfbe6647
Ernest Guevarra <ernest@validmeasures.org>
2018-06-01 07:57:10 +0100

    update addWGSR.R

--- R/addWGSR.R
+++ R/addWGSR.R
@@ -215,12 +215,28 @@ addWGSR <- function(data, sex, firstPart, secondPart, thirdPart = NA, index = NA
 #' @return A numeric value or vector of z-scores for the specified \code{index}.
 #'
 #' @examples
-#' # Calculate weight-for-height for a male child age 10 months with a weight of
-#' # 5.7 kgs and a height of 64.2 cms.
+#' # Given a male child 10 months old with a weight of 5.7 kgs, height of 64.2
+#' # cms, and MUAC of 125 mm:
+#' #
+#' # Calculate weight-for-height
 #' getWGSR(sex = 1,
 #'         firstPart = 5.7,
 #'         secondPart = 64.2,
 #'         index = "wfh",
+#'         standing = 3)
+#'
+#' # calculate weight-for-age
+#' getWGSR(sex = 1,
+#'         firstPart = 5.7,
+#'         secondPart = 10,
+#'         index = "wfa",
+#'         standing = 3)
+#'
+#' # calculate height-for-age
+#' getWGSR(sex = 1,
+#'         firstPart = 64.2,
+#'         secondPart = 10,
+#'         index = "hfa",
 #'         standing = 3)
 #'
 #' @export

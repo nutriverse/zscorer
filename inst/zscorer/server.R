3de0d8982c5a3cbd15e6c777779a48f94a6fa4f7
Ernest Guevarra <ernest@validmeasures.org>
2018-11-26 04:38:20 +0000

    update shiny

--- inst/zscorer/server.R
+++ inst/zscorer/server.R
@@ -34,8 +34,8 @@ function(input, output, session) {
     if(input$dataType == 1){
       selectInput(inputId = "sex1",
                   label = "Sex",
-                  choices = list("Male" = 1, "Female" = 2),
-                  selected = NULL)
+                  choices = list("Select" = ., "Male" = 1, "Female" = 2),
+                  selected = .)
     }
   })
   #

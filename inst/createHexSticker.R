ca50e5cb9054176aefdc3c3582e318198b7575bb
Ernest Guevarra <ernest@validmeasures.org>
2018-12-24 07:06:54 +0000

    add createHexSticker.R

---
+++ inst/createHexSticker.R
@@ -0,0 +1,7 @@
+library(hexSticker)
+
+sticker("inst/figures/growthChart.png",
+        s_x = 1, s_y = 0.9, s_width = 0.5,
+        package = "zscorer", p_color = "#E3BB00", p_y = 1.6,
+        h_size = 1, h_color = "#E3BB00", h_fill = NA,
+        filename = "man/figures/zscorer.png")

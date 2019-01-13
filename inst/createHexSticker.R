0e6d0ebcd4d981c31e848bc4f276497179134016
Ernest Guevarra <ernest@validmeasures.org>
2019-01-13 06:51:50 +0000

    update createHexSticker.R

--- inst/createHexSticker.R
+++ inst/createHexSticker.R
@@ -1,7 +1,9 @@
 library(hexSticker)
 
 sticker("inst/figures/growthChart.png",
-        s_x = 1, s_y = 0.9, s_width = 0.5,
-        package = "zscorer", p_color = "#E3BB00", p_y = 1.6,
-        h_size = 1, h_color = "#E3BB00", h_fill = NA,
+        s_x = 1, s_y = 0.9, s_width = 0.55,
+        package = "zscorer", p_color = "#E3BB00", p_y = 1.6, p_size = 6,
+        h_size = 1.5, h_color = "#E3BB00", h_fill = NA,
+        url = "https://nutriverse.io/zscorer",
+        u_size = 1, u_color = "#E3BB00", u_y = 0.08,
         filename = "man/figures/zscorer.png")

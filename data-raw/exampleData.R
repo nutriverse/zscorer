a952190ae329775f3b90c36fc3fae6df2595ce6d
Ernest Guevarra <ernest.guevarra@post.harvard.edu>
2019-10-16 13:01:55 +0100

    update exampleData.R

--- data-raw/exampleData.R
+++ data-raw/exampleData.R
@@ -1,9 +1,11 @@
-
 anthro1 <- read.csv("data-raw/dist.ex01.csv")
-devtools::use_data(anthro1, overwrite = TRUE)
+usethis::use_data(anthro1, compress = "xz", overwrite = TRUE)
 
 anthro2 <- read.csv("data-raw/dp.ex01.csv")
-devtools::use_data(anthro2, overwrite = TRUE)
+usethis::use_data(anthro2, compress = "xz", overwrite = TRUE)
 
 anthro3 <- read.csv("data-raw/z.ex01.csv")
-devtools::use_data(anthro3, overwrite = TRUE)
+usethis::use_data(anthro3, compress = "xz", overwrite = TRUE)
+
+anthro4 <- read.csv("data-raw/for_Mark.csv")
+usethis::use_data(anthro4, compress = "xz", overwrite = TRUE)
